1011d44701bdcf538048ed8d91f4a912faa0e52c
scie <emabicova@seznam.cz>
2025-03-06 11:59:37 +0100

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-teal-800 p-6 relative bottom-0 width-[100%]">
+        <div className="text-center">
+            <p className="text-white text-xs">© Bleon, Ema, Jacques</p>
+        </div>
+
+    </footer>
+  );
+}
+
+export default Footer;
